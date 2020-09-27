b98067ba67122177b3a2ea1294bf6dbb42672f62
Gorka <gorka@540deg.com>
2020-09-27 11:20:40 +0200

    Solved with basic requeriments

--- src/fizzbuzz.ts
+++ src/fizzbuzz.ts
@@ -1,1 +1,17 @@
-export const fizzBuzz = () => undefined;
+export const fizzBuzz = (aNumber: number) => {
+    if(isDivisibleBy3(aNumber) && isDivisibleBy5(aNumber)) {
+        return 'FizzBuzz'
+    }
+    if(isDivisibleBy3(aNumber)) {
+        return 'Fizz'
+    }
+    if(isDivisibleBy5(aNumber)) {
+        return 'Buzz'
+    }
+    
+    return aNumber.toString();
+}
+
+const isDivisibleBy = (divisor: number) => (n: number) => n%divisor === 0
+const isDivisibleBy3 = isDivisibleBy(3)
+const isDivisibleBy5 = isDivisibleBy(5)
--- test/fizzbuzz.test.ts
+++ test/fizzbuzz.test.ts
@@ -1,7 +1,35 @@
 import { fizzBuzz } from '../src/fizzbuzz'
 
 describe('FizzBuzz', () => {
-    it('example test', () => {
-        expect(fizzBuzz()).toEqual(undefined)
+    it('returns "1" for 1 number', () => {
+        expect(fizzBuzz(1)).toEqual('1')
+    })
+
+    it('returns "2" for 2 number', () => {
+        expect(fizzBuzz(2)).toEqual('2')
+    })
+
+    it('returns "Fizz" for 3 number', () => {
+        expect(fizzBuzz(3)).toEqual('Fizz')
+    })
+
+    it('returns "Fizz" for 6 number', () => {
+        expect(fizzBuzz(6)).toEqual('Fizz')
+    })
+
+    it('returns "Buzz" for 5 number', () => {
+        expect(fizzBuzz(5)).toEqual('Buzz')
+    })
+
+    it('returns "Buzz" for 10 number', () => {
+        expect(fizzBuzz(10)).toEqual('Buzz')
+    })
+
+    it('returns "Buzz" for 5 number', () => {
+        expect(fizzBuzz(5)).toEqual('Buzz')
+    })
+
+    it('returns "FizzBuzz" for 15 number', () => {
+        expect(fizzBuzz(15)).toEqual('FizzBuzz')
     })
 })
