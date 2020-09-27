f519846c954263706f6922bc6df1434070b9e218
Gorka <gorka@540deg.com>
2020-09-27 14:03:34 +0200

    One to many step 2

--- test/fizzbuzz.test.ts
+++ test/fizzbuzz.test.ts
@@ -4,46 +4,48 @@ describe('FizzBuzz', () => {
     const fizzBuzz = createFizzBuzz();
 
     it('returns "1" for 1 number', () => {
-        expect(fizzBuzz(1)).toEqual('1');
+        expect(runFizzBuzz(1)).toEqual('1');
     });
 
     it('returns "2" for 2 number', () => {
-        expect(fizzBuzz(2)).toEqual('2');
+        expect(runFizzBuzz(2)).toEqual('2');
     });
 
     it('returns "Fizz" for 3 number', () => {
-        expect(fizzBuzz(3)).toEqual('Fizz');
+        expect(runFizzBuzz(3)).toEqual('Fizz');
     });
 
     it('returns "Fizz" for 6 number', () => {
-        expect(fizzBuzz(6)).toEqual('Fizz');
+        expect(runFizzBuzz(6)).toEqual('Fizz');
     });
 
     it('returns "Buzz" for 5 number', () => {
-        expect(fizzBuzz(5)).toEqual('Buzz');
+        expect(runFizzBuzz(5)).toEqual('Buzz');
     });
 
     it('returns "Buzz" for 10 number', () => {
-        expect(fizzBuzz(10)).toEqual('Buzz');
+        expect(runFizzBuzz(10)).toEqual('Buzz');
     });
 
     it('returns "Buzz" for 5 number', () => {
-        expect(fizzBuzz(5)).toEqual('Buzz');
+        expect(runFizzBuzz(5)).toEqual('Buzz');
     });
 
     it('returns "FizzBuzz" for 15 number', () => {
-        expect(fizzBuzz(15)).toEqual('FizzBuzz');
+        expect(runFizzBuzz(15)).toEqual('FizzBuzz');
     });
 
     it('returns "Fizz" for number containing 3', () => {
-        expect(fizzBuzz(35)).toEqual('Fizz');
+        expect(runFizzBuzz(35)).toEqual('Fizz');
     });
 
     it('returns "Buzz" for number containing 5', () => {
-        expect(fizzBuzz(53)).toEqual('Buzz');
+        expect(runFizzBuzz(53)).toEqual('Buzz');
     });
 
     it('returns multiple numbers', () => {
         expect(fizzBuzz(53, [1, 2, 3, 4, 5])).toEqual(['1', '2', 'Fizz', '4', 'Buzz']);
     });
+
+    const runFizzBuzz = (n: number) => fizzBuzz(n, [n])[0]
 });
