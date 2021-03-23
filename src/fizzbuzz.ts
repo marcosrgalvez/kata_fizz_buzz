dbf93eea16524c6a963b65fcd83c92fa58352213
Marcos Galvez <marcos.galvez@biko2.com>
2021-03-23 10:37:05 +0100

    When the number is 5 returns Buzz

--- src/fizzbuzz.ts
+++ src/fizzbuzz.ts
@@ -1,1 +1,8 @@
-export const fizzBuzz = (number:number) => number === 3 ? 'Fizz' : number;
+export const fizzBuzz = (number:number) => {
+    if (number === 3){
+        return 'Fizz'
+    } else if (number === 5){
+        return 'Buzz'
+    } else return number
+}
+
--- test/fizzbuzz.test.ts
+++ test/fizzbuzz.test.ts
@@ -1,11 +1,14 @@
 import { fizzBuzz } from '../src/fizzbuzz'
 
 describe('Fizz Buzz', () => {
-    it('If the number is not 3 returns Fizz', () => {
+    it('If the number is not 3 niether 5 returns the number', () => {
         expect(fizzBuzz(1)).toEqual(1)
         expect(fizzBuzz(2)).toEqual(2)
     })
     it('If the number is 3 returns Fizz', () => {
         expect(fizzBuzz(3)).toEqual('Fizz')
+    })
+    it('If the number is 5 returns Buzz', () => {
+        expect(fizzBuzz(5)).toEqual('Buzz')
     })
 })
