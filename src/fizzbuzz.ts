49fa3388228462c102b3af701fe50328088c0023
Marcos Galvez <marcos.galvez@biko2.com>
2021-03-23 10:49:47 +0100

    Refactoring the las commit

--- src/fizzbuzz.ts
+++ src/fizzbuzz.ts
@@ -1,12 +1,16 @@
 export const fizzBuzz = (number:number) => {
     if (divisibleByThree(number)){
         return 'Fizz'
-    } else if (number % 5 === 0){
+    } else if (divisibleByFive(number)){
         return 'Buzz'
     } else return number
 }
 
 let divisibleByThree = function(number:number) : boolean{
     return number % 3 === 0
+}
+
+let divisibleByFive = function(number:number) : boolean{
+    return number % 5 === 0
 }
 
